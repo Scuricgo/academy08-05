c2100457ba25b9dfd7ff6e09c99c3237a1786de1
scuricgo <goran.schuric@gmail.com>
2022-05-08 14:09:18 +0200

    I finished exercise 2

---
+++ Vježba2.dart
@@ -0,0 +1,13 @@
+double temperature = 25;
+int value = 3;
+String pizza = 'pizza';
+String pasta = 'pasta';
+int zbroj = value + value;
+
+
+void main() {
+  
+    print("The temperature is $temperature C");
+  print("$value plus $value makes $zbroj");
+  print("I like $pizza and $pasta");
+  }
