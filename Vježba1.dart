98349253bdfa04ca92139e0cf29db05ed18f6a1f
scuricgo <goran.schuric@gmail.com>
2022-05-08 13:51:11 +0200

    I finished 1. erxercise

---
+++ Vježba1.dart
@@ -0,0 +1,13 @@
+String firstname = "Vedran";
+String lastname = "Balagovic";
+num age = 38;
+double height = 1.75;
+  
+
+void main() {
+
+    print(firstname);
+  print(lastname);
+  print(age);
+  print(height);
+  }
